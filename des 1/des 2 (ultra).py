1ca6015472763f7137ef0180fe8a218fef64d501
Gabriel Lucas m. f <71914185+yuriluisz@users.noreply.github.com>
2022-09-19 16:34:03 -0400

    Update des 2 (ultra).py

--- des 1/des 2 (ultra).py
+++ des 1/des 2 (ultra).py
@@ -2,29 +2,38 @@ d = int(input("Insira o dia do seu nascimento: "))
 m = int(input("Insira o mes do seu nascimento: "))
 a = int(input("Insira o ano do seu nascimento: "))
 
-#print("A data do seu nacsimento e {} / {} / {}".format(d, m, a))
+if d > 31:
+    print("O Dia que voce inseriu ultrapassa os limites!")
 
-if m == 1:
-    print("A data do seu nacsimento formatada e:", d, "/ janeiro /", a)
+if m > 12:
+    print("O Mes que voce inseriu ultrapassa os limites!")
+
+
+
+elif m == 1:
+    print("A data do seu nascimento formatada e:", d, "/ janeiro /", a)
 elif m == 2:
-    print("A data do seu nacsimento formatada e:", d, "/ fevereiro /", a)
+    print("A data do seu nascimento formatada e:", d, "/ fevereiro /", a)
 elif m == 3:
-    print("A data do seu nacsimento formatada e:", d, "/ marco /", a) 
+    print("A data do seu nascimento formatada e:", d, "/ marco /", a) 
 elif m == 4:
-    print("A data do seu nacsimento formatada e:", d, "/ abril /", a)
+    print("A data do seu nascimento formatada e:", d, "/ abril /", a)
 elif m == 5:
-    print("A data do seu nacsimento formatada e:", d, "/ maio /", a)
+    print("A data do seu nascimento formatada e:", d, "/ maio /", a)
 elif m == 6:
-    print("A data do seu nacsimento formatada e:", d, "/ junho /", a)
+    print("A data do seu nascimento formatada e:", d, "/ junho /", a)
 elif m == 7:
-    print("A data do seu nacsimento formatada e:", d, "/ julho /", a)
+    print("A data do seu nascimento formatada e:", d, "/ julho /", a)
 elif m == 8:
-    print("A data do seu nacsimento formatada e:", d, "/ agosto /", a)
+    print("A data do seu nascimento formatada e:", d, "/ agosto /", a)
 elif m == 9:
-    print("A data do seu nacsimento formatada e:", d, "/ setembro /", a)
+    print("A data do seu nascimento formatada e:", d, "/ setembro /", a)
 elif m == 10:
-    print("A data do seu nacsimento formatada e:", d, "/ outubro /", a)
+    print("A data do seu nascimento formatada e:", d, "/ outubro /", a)
 elif m == 11:
-    print("A data do seu nacsimento formatada e:", d, "/ novembro /", a)
+    print("A data do seu nascimento formatada e:", d, "/ novembro /", a)
 elif m == 12:
-    print("A data do seu nacsimento formatada e:", d, "/ dezembro /", a)
+    print("A data do seu nascimento formatada e:", d, "/ dezembro /", a)
+
+
+#print("A data do seu nacsimento e {} / {} / {}".format(d, m, a))
