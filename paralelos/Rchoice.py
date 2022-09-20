2ff09fb845f63656e2202940731a4614b8b4a865
Yuri Luis <71914185+yuriluisz@users.noreply.github.com>
2022-09-19 21:49:35 -0400

    failed
    tentativa falha de adicionar pysimplegui nisto

--- paralelos/Rchoice.py
+++ paralelos/Rchoice.py
@@ -1,10 +1,12 @@
 import random
 
-nm = int(input("Insira o numero de alunos PRESENTES: "))
+nm = int(input("Insira o numero de alunos PRESENTES! "))
 
 alunos = []
 while len(alunos) < nm:
-    nomes = input("Nome do Aluno: ")
+    nomes = input("Insira os nomes dos alunos: ")
     alunos.append(nomes)
 
 print("O aluno sorteado para ir a frente apresentar foi: {}".format(random.choice(alunos)))
+
+
